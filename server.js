26e2ba57d725d445637a98bc816ffb16becd7e40
Joshua Mines <minesjoshua@gmail.com>
2021-06-29 16:01:54 -0600

    asdf

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cors = require('cors')
 const Rollbar = require("rollbar");
+const send = require('send');
 const rollbar = new Rollbar({
   accessToken: '9e9dd72f3e6b487cb059b1611850ccaa',
   captureUncaught: true,
@@ -16,6 +18,13 @@ app.use(express.json())
 app.get('/', function(req, res) {fakeFunction();
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
+
+let word = ['hot', 'dog', 'cat']
+
+app.get('/api/word/', function (req, res){
+    res.status(200).send(word);
+});
+
 
 
 const port = process.env.PORT || 4000

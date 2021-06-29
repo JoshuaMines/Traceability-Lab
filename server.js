284924a4e0aba34060c77466b818f29672cef26f
Joshua Mines <minesjoshua@gmail.com>
2021-06-29 14:24:21 -0600

    hello

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-var Rollbar = require("rollbar");
-var rollbar = new Rollbar({
+const Rollbar = require("rollbar");
+const rollbar = new Rollbar({
   accessToken: '9e9dd72f3e6b487cb059b1611850ccaa',
   captureUncaught: true,
   captureUnhandledRejections: true
 });
 
-
+rollbar.log("Hello worldzz!");
 
 app.use(express.json())
 
 
 app.get('/', function(req, res) {
-    rollbar.log("Hello world!");
+    
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
